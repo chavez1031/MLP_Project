7f19a9cc9c0ffb4a5d26fe506f9ed2234b735bd5
porterj091 <joporter5606@live.com>
2017-03-17 12:35:05 -0700

    changed preprocessing

--- regression.py
+++ regression.py
@@ -11,14 +11,15 @@ def preprocessFacebook(fileIn, fileOut):
     with open(fileIn, 'r') as fin:
         outF = open(fileOut, 'w')
         for data in fin:
-            if data.find('Photo') > -1:
-                outF.write(data.replace('Photo', Photo).replace('\r', ''))
-            elif data.find('Status') > -1:
-                outF.write(data.replace('Status', Status).replace('\r', ''))
-            elif data.find('Link') > -1:
-                outF.write(data.replace('Link', Link).replace('\r', ''))
-            elif data.find('Video') > -1:
-                outF.write(data.replace('Video', Video).replace('\r', ''))
+            if data.find(';;') == -1:
+                if data.find('Photo') > -1:
+                    outF.write(data.replace('Photo', Photo).replace('\r', ''))
+                elif data.find('Status') > -1:
+                    outF.write(data.replace('Status', Status).replace('\r', ''))
+                elif data.find('Link') > -1:
+                    outF.write(data.replace('Link', Link).replace('\r', ''))
+                elif data.find('Video') > -1:
+                    outF.write(data.replace('Video', Video).replace('\r', ''))
 
         outF.close()
 
@@ -32,8 +33,6 @@ def main():
         for i in f:
             data.append(i.replace('\n', '').split(';'))
 
-
-    print data[:3]
     print np.shape(data)
     data = np.loadtxt('facebook.csv', delimiter = ';')
 
@@ -41,8 +40,11 @@ def main():
     testSet = data[250:375]
     validSet = data[375:500]
 
-    net = mlp.mlp(trainSet, trainSet, 3, outtype = 'linear')
-    net.mlptrain(trainSet, trainSet, 0.4, 500)
+    print np.shape(trainSet[:,0:18])
+    print np.shape(trainSet[:,18:19])
+    net = mlp.mlp(trainSet[:,:18], trainSet[:,18:19], 4, outtype = 'linear')
+
+    net.mlptrain(trainSet, trainSet, 0.25, 500)
 
 
 
